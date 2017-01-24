825ad2d62166f6d2811ae35e752b0ead6eec97cc
Masereka Cleave <cmasereka@gmail.com>
2017-01-24 03:04:59 -0600

    Added checks on the login form to check if the fields are empty or match with some specific credentials

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,72 @@
 package application;
-	
+
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
-
 public class Main extends Application {
+	
+	@FXML
+	private TextField username;
+	@FXML
+	private TextField password;
+	
 	@Override
 	public void start(Stage primaryStage) {
+
+		System.out.println("Testing Action listener");
 		try {
 			Pane root = (Pane) FXMLLoader.load(Main.class.getResource("LoginForm.fxml"));
-			Scene scene = new Scene(root,530,350);
+			Scene scene = new Scene(root, 530, 350);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
+	@FXML
+	private void handleLoginButtonAction() {
+		String uname = username.getText();
+		String upass = password.getText();
+		System.out.println("Username: " + uname + " Pass: " + upass);
+		
+		if(uname.trim().length() == 0 || upass.trim().length() == 0){
+			alertMessage("Fill all the required Fields");
+		}else if(uname.trim() != "test" && upass.trim() != "123"){
+			alertMessage("Invalid Username or Password");
+		}else{
+			alertMessage("Login Successfull");
+		}
+	}
 	
+	private void alertMessage(String msg){
+		Alert alert = new Alert(AlertType.WARNING, msg, ButtonType.OK);
+		alert.showAndWait();
+	}
+
+	@FXML
+	private void handleCancelButtonAction() {
+		System.out.println("Testing Action listener");
+		Alert alert = new Alert(AlertType.CONFIRMATION,
+				"You are about to close this Application. Would you like to close it?", ButtonType.YES, ButtonType.NO);
+		alert.showAndWait();
+
+		if (alert.getResult() == ButtonType.YES) {
+			Platform.exit();
+		}
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
